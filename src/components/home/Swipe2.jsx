5ee4318badd00e4691a21158bd8a599121101e27
redjuncell <714189654@qq.com>
2020-05-03 22:00:37 +0800

    202053首页进度

--- src/components/home/Swipe2.jsx
+++ src/components/home/Swipe2.jsx
@@ -20,40 +20,81 @@ class SWipe2 extends React.Component{
     }
     return (
       <div className={swipe.content}>
-        <ReactSwipe className="carousel" swipeOptions={opt}>
-          <div className={swipe.page}>
-            <ul className='clear-fix'>
-                 1
+        <ReactSwipe className="carousel" swipeOptions={opt}> 
+            <div className={swipe.page}>
+                <ul className='clear-fix'>
+                    <a href="">
+                    <li>
+                        <div className={swipe.li_left}>
+                                    <img src="https://image.juooo.com/group1/M00/03/2E/rAoKmV2tZOeADHvUAACprrXJhIE919.jpg" alt=""/>
+                                </div>
+                                <div className={swipe.li_right}>
+                                    <h3>浪漫经典《美女与野兽》</h3>
+                                    <div className={swipe.p}>
+                                        <div>
+                                            <span>5</span>折起
+                                    </div>
+                                    <div>
+                                        <button>立即抢购</button>
+                                    </div>
+                                </div>
+                            </div>
+                     </li>
+                </a>
             </ul>
           </div>
-          <div className={swipe.page}>
+        <div className={swipe.page}>
             <ul className='clear-fix'>
-                5
-            </ul>
-          </div>
-          <div className={swipe.page}>
+                <a href="">
+                <li>
+                    <div className={swipe.li_left}>
+                                <img src="https://image.juooo.com/group1/M00/03/38/rAoKmV2_wIuAWsXBAADPA6clalw098.jpg" alt=""/>
+                            </div>
+                            <div className={swipe.li_right}>
+                                <h3>浪漫经典《美女与野兽》</h3>
+                                <div className={swipe.p}>
+                                    <div>
+                                        <span>5</span>折起
+                                    </div>
+                                    <div>
+                                        <button>立即抢购</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </a>
+                </ul>
+            </div>
+        <div className={swipe.page}>
             <ul className='clear-fix'>
-                3
-            </ul>
-          </div>
-          <div className={swipe.page}>
-            <ul className='clear-fix'>
-                4
-            </ul>
-          </div>
-          
+                <a href="">
+                <li>
+                    <div className={swipe.li_left}>
+                                <img src="https://image.juooo.com/group1/M00/03/D5/rAoKNV2m0miATupUAACePEfinjA021.jpg" alt=""/>
+                            </div>
+                            <div className={swipe.li_right}>
+                                <h3>浪漫经典《美女与野兽》</h3>
+                                <div className={swipe.p}>
+                                    <div>
+                                        <span>5</span>折起
+                                    </div>
+                                    <div>
+                                        <button>立即抢购</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </a>
+                </ul>
+            </div>
+         
         </ReactSwipe>
         <div className={swipe.index}>
           <ul>
             <li className={this.state.index === 0 ? swipe.selected: ''}></li>
             <li className={this.state.index === 1 ? swipe.selected: ''}></li>
             <li className={this.state.index === 2 ? swipe.selected: ''}></li>
-            <li className={this.state.index === 3 ? swipe.selected: ''}></li>
-            <li className={this.state.index === 4 ? swipe.selected: ''}></li>
-            <li className={this.state.index === 5 ? swipe.selected: ''}></li>
-            <li className={this.state.index === 5 ? swipe.selected: ''}></li>
-            <li className={this.state.index === 7 ? swipe.selected: ''}></li>
-            <li className={this.state.index === 8 ? swipe.selected: ''}></li>
+            
           </ul>
         </div>
       </div>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,6 +3,7 @@ import  axios from  'axios'
 import  home  from  '../assets/css/home/home.module.scss'
 import  Swipe from  '../components/home/Swipe'
 import  Swipe2  from  '../components/home/Swipe2'
+import  HotSlider  from '../components/home/Hotslider'
 import  action  from  '../store/actionType/swipe'
 import  getHomeData from  '../store/actionCreator/home/home'
 import {
@@ -34,39 +35,53 @@ class Home extends React.Component{
                   <div className={home.swipe}>
                       <Swipe></Swipe>
                   </div>
-                    <section className={home.content}>
-                        <div className={home.top}>
-                             {
-                                this.props.data.classify_list.map(v=>(
-                                    <div className={home.menu} key={v.id}>
-                                        <a href='/ticket'>
-                                            <div className={home.menuUi}>
-                                                <img src={v.pic} alt=""/>
-                                            </div>
-                                            <div className={home.menuText}>{v.name}</div>
-                                        </a>
-                                    </div>
-                                ))
-                                } 
+                <section className={home.content}>
+                    <div className={home.top}>
+                            {
+                            this.props.data.classify_list.map(v=>(
+                                <div className={home.menu} key={v.id}>
+                                    <a href='/ticket'>
+                                        <div className={home.menuUi}>
+                                            <img src={v.pic} alt=""/>
+                                        </div>
+                                        <div className={home.menuText}>{v.name}</div>
+                                    </a>
+                                </div>
+                            ))
+                            } 
+                    </div>
+                    <div className={home.section_center}>
+                            <a href="">
+                                <div className={home.section_center_top}>
+                                        <div className={home.top_left}>
+                                                <i><img className={home.img} src={img1} alt=""/></i>
+                                                <span>会员专享折扣</span>
+                                        </div>
+                                        <div className={home.top_right}>
+                                            <span>99元/年</span>
+                                        </div>
+                                </div>
+                            </a>
+                        <div className={home.section_center_bottom}>
+                            <Swipe2></Swipe2>
                         </div>
-                        <div className={home.section_center}>
-                                <a href="">
-                                    <div className={home.section_center_top}>
-                                            <div className={home.top_left}>
-                                                    <i><img className={home.img} src={img1} alt=""/></i>
-                                                    <span>会员专享折扣</span>
-                                            </div>
-                                            <div className={home.top_right}>
-                                                <span>99元/年</span>
-                                            </div>
-                                    </div>
-                                </a>
-                            <div className={home.section_center_bottom}>
-                                <Swipe2></Swipe2>
+                    </div>
+                    <div className={home.section_bottom}>
+                        <img src="https://image.juooo.com/group1/M00/03/96/rAoKmV6W4RyAdkouAAJa2-GtD74165.png" alt=""/>
+                    </div>
+                </section>
+                <div className={home.hotslider}>
+                        <div className={home.hotslider_top}>
+                            <h3>热门演出</h3>
+                            <div>
+                                <span>全部</span>
+                                <i>> </i>
                             </div>
                         </div>
-                        <div className={home.bottom}></div>
-                    </section>
+                        <HotSlider></HotSlider>
+                    </div>
+            
+            <div className={home.cheng}></div>
             </div>
         )
     }
