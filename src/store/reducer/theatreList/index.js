1339277ce31406748e9eabf530a42fee9b81a3b8
xiexiaoyun <super_yun93@163.com>
2020-05-06 21:37:29 +0800

    完成剧院页面

--- src/store/reducer/theatreList/index.js
+++ src/store/reducer/theatreList/index.js
@@ -1,10 +1,12 @@
 import  theatreListType from '../../actionType/theatreList'
-import  theatreListInit  from  '../../state/theatreList'
-export default  function (state={theatreListInit},{type,payload}){
-    console.log(payload)
+import  theatre_list  from  '../../state/theatreList'
+export default  function (state=theatre_list,{type,payload}){
+    // console.log(payload)
     state = JSON.parse(JSON.stringify(state));
      if(type===theatreListType.GET_THEATRELIST){
-        //  state:payload
+        //  state.theatreListInit=payload.theatreListInit;
+        state.theatre_list = payload.theatre_list;
+
      }
      return state
 }
--- src/views/Index.jsx
+++ src/views/Index.jsx
@@ -13,8 +13,8 @@ import {connect}  from  'react-redux'
          }
      }
     render() {
-         console.log(this.props.history,2222222222)
-         console.log(this.props.home.classify,33333333333)
+        //  console.log(this.props.history,2222222222)
+        //  console.log(this.props.home.classify,33333333333)
         return (
             <div>
                 <MyRouter router={this.props.childrens}></MyRouter>
